f2df547b514f86c0b8d8d2ef67fad2513b48cec4
asma <asma.zguem@insat.ucar.tn>
2025-02-01 20:43:36 +0100

    some other changes

--- src/app/patient/patient-appointments/interfaces/appointment.ts
+++ src/app/patient/patient-appointments/interfaces/appointment.ts
@@ -1,10 +1,9 @@
 import { Doctor } from "../../doctors/interfaces/doctor";
 
-export interface Appointment {
-    id: number;  // ✅ Fix missing ID issue
-    doctor: Doctor;
-    status: string;
-    payed: boolean;
-    date: Date;
+export interface Appointment{
+    doctor:Doctor,
+    status:string,
+    payed:boolean,
+    date:Date
     session:number
 }
